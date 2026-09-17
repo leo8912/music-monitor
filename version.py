4d913435bf22106043ea09df5934fa701267f8f8
Lao <474482084@qq.com>
2026-09-17 12:56:47 +0800

    2.11.3

--- version.py
+++ version.py
@@ -3,10 +3,10 @@
 自动读取并提供版本号信息
 """
 
-__backend_version__ = "2.11.2"
-__frontend_version__ = "2.11.2"
+__backend_version__ = "2.11.3"
+__frontend_version__ = "2.11.3"
 # 自动生成的构建时间 (Updated by Agent)
-__build_date__ = "2026-09-17 12:45:00"
+__build_date__ = "2026-09-17 12:50:00"
 
 def get_version_info():
     """获取版本信息"""
